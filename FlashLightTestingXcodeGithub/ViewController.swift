131611ea4d90472790d2134baefd6cf7ba48a269
Margiett Gil <mxg627@icloud.com>
2019-10-29 11:15:02 -0400

    Initial Commit

---
+++ FlashLightTestingXcodeGithub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashLightTestingXcodeGithub
+//
+//  Created by Margiett Gil on 10/29/19.
+//  Copyright © 2019 Margiett Gil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
